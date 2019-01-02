e0d9a0b95bb962d72693e80169d9df69005feefc
khufkens <koen.hufkens@gmail.com>
2019-01-02 15:25:03 +0100

    suggests checks on the explorer GUI

--- R/snotel_explorer.r
+++ R/snotel_explorer.r
@@ -6,6 +6,15 @@
 #' # snotel_explorer()
 
 snotel_explorer <- function(){
+  if(!requireNamespace(c("DT",
+                         "plotly",
+                         "shinydashboard",
+                         "leaflet"), quietly = TRUE)){
+    stop("Packages \"DT, plotly, shinydashboard and leaflet\" are needed 
+         for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  
   appDir <- sprintf("%s/shiny/snotel_explorer",path.package("snotelr"))
   shiny::runApp(appDir, display.mode = "normal",launch.browser=TRUE)
 }
